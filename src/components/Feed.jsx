edbf3df66e4223e38b7bf7872518152e1cc7472f
Brian Sai <brianwsai@gmail.com>
2023-06-01 20:33:45 -0700

    Fetched from youtube api using rapidapi

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import SideBar from './SideBar';
+import { SideBar, Videos } from './';
+import { fetchfromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  useEffect(() => {
+    fetchfromAPI(`search/?part=snippet&q=${selectedCategory}`);
+  }, []);
   return (
     <Stack sx={{ flexDirection: { sx: 'colum', md: 'row' } }}>
       <Box
@@ -12,7 +17,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <SideBar />
+        <SideBar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -20,6 +28,17 @@ const Feed = () => {
         >
           Copyright 2023 YUM Media
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          New <span style={{ color: '#F31503' }}>videos</span>
+        </Typography>
+        <Videos videos={[]} />
       </Box>
     </Stack>
   );
